5606f7f3a89c06c22b80e7546a802cb214215416
tienthanh11 <dotienthanh1121@gmail.com>
2023-04-16 01:33:00 +0700

    module 5

--- Module_5/CaseStudy_Module5/angular-casestudy/src/app/app.module.ts
+++ Module_5/CaseStudy_Module5/angular-casestudy/src/app/app.module.ts
@@ -7,6 +7,17 @@ import { HeaderComponent } from './templates/header/header.component';
 import { NavigationComponent } from './templates/navigation/navigation.component';
 import { FooterComponent } from './templates/footer/footer.component';
 import { IntroComponent } from './templates/intro/intro.component';
+import { FacilitiesListComponent } from './facilities/facilities-list/facilities-list.component';
+import { FacilitiesCreateComponent } from './facilities/facilities-create/facilities-create.component';
+import { FacilitiesEditComponent } from './facilities/facilities-edit/facilities-edit.component';
+import { CustomerListComponent } from './customer/customer-list/customer-list.component';
+import { CustomerCreateComponent } from './customer/customer-create/customer-create.component';
+import { CustomerEditComponent } from './customer/customer-edit/customer-edit.component';
+import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
+import { EmployeeCreateComponent } from './employee/employee-create/employee-create.component';
+import { EmployeeEditComponent } from './employee/employee-edit/employee-edit.component';
+import { ContractListComponent } from './contract/contract-list/contract-list.component';
+import { ContractCreateComponent } from './contract/contract-create/contract-create.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +25,18 @@ import { IntroComponent } from './templates/intro/intro.component';
     HeaderComponent,
     NavigationComponent,
     FooterComponent,
-    IntroComponent
+    IntroComponent,
+    FacilitiesListComponent,
+    FacilitiesCreateComponent,
+    FacilitiesEditComponent,
+    CustomerListComponent,
+    CustomerCreateComponent,
+    CustomerEditComponent,
+    EmployeeListComponent,
+    EmployeeCreateComponent,
+    EmployeeEditComponent,
+    ContractListComponent,
+    ContractCreateComponent
   ],
   imports: [
     BrowserModule,
